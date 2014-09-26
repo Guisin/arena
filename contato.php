b4cead5a0f2fb56f82fd3abaeb0144229064b88b
Guisin <gdpguisin@hotmail.com>
2014-09-26 08:30:46 -0300

    ENVIO DE EMAIL
    Envio de e-mail funcionando.

--- contato.php
+++ contato.php
@@ -2,30 +2,25 @@
 	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="js/validate/jquery.validate.js"></script>
     <script type="text/javascript" src="js/valida_form.js"></script>
-    <?
-	include "function/function.php";
-	$texto = "Nome cliente: " . $_POST['nome']
-			 . "Telefone cliente: " . $_POST['telefone'] .
-			 "Celular cliente: " . $_POST['celular'] .
-			 "E-mail cliente: " . $_POST['email'] .
-			 "Solicitação cliente" . $_POST['informacao'];
-	if(strlen($_POST['nome']))
-	{
-		if(sendMail($_POST['email'],'suporte02@automacaodovale.com.br', $texto, 'Formulário de contato Site'))
-		{
-			echo "Sua mensagem foi enviada com sucesso!";
+<?php
+	
+	if($_GET){		
+		if($_GET['env'] =='True'){
+			require_once 'function/function.php';
+			if(sendMail($_POST['nome'], $_POST['email'], $_POST['mensagem'])){
+				echo 'Mensagem enviada';
+			}else{
+				echo 'Mensagem não enviada';
+			}
 		}
-		else
-		{
-			echo "Ocorreu um erro ao enviar";
-		}
-		echo "<br><a href='index.php'>Voltar</a>";
-		exit();
-	}?>
+	}
+	
+	
+?>
 <?php require_once "menu.php" ?>
 			<div id="box-form-contato">
            	  <h1>Contato</h1>
-            	<form id="form-contato" method="post">
+            	<form id="form-contato" method="post" action="contato.php?env=True">
                 	<label class="title">Nome*</label>
                     <input type="text" name="nome">
                     <br><br>
@@ -39,7 +34,7 @@
                     <input type="text" name="celular">
                     <br><br>
                     <label class="title">Informação*</label>
-                    <textarea name="informacao"></textarea>
+                    <textarea name="mensagem"></textarea>
                     <div class="clear"></div>
                     <br>                    
                     <button>Enviar</button>
--- function/function.php
+++ function/function.php
@@ -1,28 +1,20 @@
 <?php
-function sendMail($de,$para,$mensagem,$assunto)
+function sendMail($nome,$email,$mensagem)
 {
     require_once('function/PHPMailer/PHPMailerAutoload.php');
-    $mail = new PHPMailer(true);
-    $mail->IsSMTP();
-    try {
-      $mail->SMTPAuth   = true;
-      $mail->Host       = 'smtp.automacaodovale.com.br';
-      #$mail->SMTPSecure = "tls"; #remova se nao usar gmail
-     $mail->Port       = 587;                  #remova se nao usar gmail
-      $mail->Username   = 'suporte02@automacaodovale.com.br';
-      $mail->Password   = 'entra123';
-      $mail->AddAddress($para);
-     $mail->AddReplyTo($de);
-     $mail->SetFrom($de);
-      $mail->Subject = $assunto;
-      $mail->MsgHTML($mensagem);
-      $mail->Send();    
-      $envio = true;
-    } catch (phpmailerException $e) {
-      $envio = false;
-    } catch (Exception $e) {
-      $envio = false;
-    }
-    return $envio;
+	$mail = new PHPMailer();
+	$mail->IsMail(true);
+	$mail->IsHTML(true);
+	$mail->From = $email;
+	$mail->FromName = $nome;
+	$mail->AddAddress('suporte02@automacaodovale.com.br','Arena Piscinas');
+	$mail->Subject = 'Mensagem - Site';
+	$mail->Body = '<b>Nome:</b> '.$nome.' <br /><br /><b>E-mail:</b> '.$email.' <br /><br /><b>Mensagem:</b> '.$mensagem;
+	if($mail->Send()){
+	 $envio = true;
+	}else{
+	 $envio = false;
+	}
+	return $envio;
 }
 ?>
